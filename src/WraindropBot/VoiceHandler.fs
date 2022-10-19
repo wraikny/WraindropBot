cd9f8c9ffb07a334bb0d6e23260bedc465e6bb95
wraikny <writtenknown@gmail.com>
2022-10-19 19:00:18 +0900

    format

--- src/WraindropBot/VoiceHandler.fs
+++ src/WraindropBot/VoiceHandler.fs
@@ -158,15 +158,15 @@ type VoiceHandler(wdConfig: WDConfig, services: ServiceProvider) =
                   || args.Author.IsBot
                   || (args.MentionedUsers.Count <> 0
                       && args.MentionedUsers
-                        |> Seq.forall (fun x -> x.IsBot))
+                         |> Seq.forall (fun x -> x.IsBot))
                   || wdConfig.commandPrefixes
-                    |> Seq.exists msg.StartsWith
+                     |> Seq.exists msg.StartsWith
                   || wdConfig.ignorePrefixes
-                    |> Seq.exists msg.StartsWith
+                     |> Seq.exists msg.StartsWith
                   || isNull conn
                   || registeredChannelId
-                    |> Option.is ((=) messageChannelId)
-                    |> not
+                     |> Option.is ((=) messageChannelId)
+                     |> not
                 )
             then
               let! user = textConverter.GetUserWithValidName(args.Guild, args.Author.Id)
