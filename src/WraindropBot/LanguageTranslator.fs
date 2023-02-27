0d9b7973c9e51eeaf0a2bb09cee5d7c6d9dd19e1
wraikny <writtenknown@gmail.com>
2023-02-28 00:22:46 +0900

    fix translator

--- src/WraindropBot/LanguageTranslator.fs
+++ src/WraindropBot/LanguageTranslator.fs
@@ -21,10 +21,9 @@ type LanguageTranslator(wdConfig: WDConfig) =
     task {
       if not urlIsValid then
         return Error "translationUrl is not valid."
-      elif
-        Utils.Language.tryFindCode (source)
-        |> Option.isNone
-      then
+      elif source <> ""
+           && Utils.Language.tryFindCode (source)
+              |> Option.isNone then
         return Error $"invalid source '%s{source}'"
       elif
         Utils.Language.tryFindCode (target)
